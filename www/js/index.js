554ef5c0e89beab1de6e86a0221571ff0b52ac2d
Emerson H V <ehvasquez@hotmail.com>
2015-04-21 15:45:04 -0500

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -230,6 +230,19 @@ var app = {
             alert(error);
         }
 
+        /*cordova.plugins.barcodeScanner.scan(
+          function (result) {
+              alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+          }, 
+          function (error) {
+              alert("Scanning failed: " + error);
+          }
+       );*/
+        
+        
         scanner.scan( function (result) {
             alert("Entrada 2");
             /*console.log("Scanner result: \n" +
