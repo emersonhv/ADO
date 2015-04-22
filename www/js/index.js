50219bdd52260db4f2fd48677a3887d428d96516
Emerson H V <ehvasquez@hotmail.com>
2015-04-22 10:44:00 -0500

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -208,7 +208,7 @@ var app = {
                         htmlNotas = "<p>"+p.nombre_clases+"<input style='width:10px;' type='text' id='"+p.id_asistencia+"' value='"+p.nota+"'/> <button>Editar</button></p>";
                         $('#NotasPorBimestre').append(htmlNotas);
                     }
-                    $('#NotasPorBimestre').trigger("create");
+                    $('#NotasPorBimestre').refresh();
                 }
             });
         } catch (error) {
