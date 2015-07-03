9737c27ef327aeeefeeeaabb312300609960c083
Emerson H V <ehvasquez@hotmail.com>
2015-07-03 09:23:06 -0500

    commit

--- www/js/index.js
+++ www/js/index.js
@@ -113,6 +113,7 @@ var app = {
                 success:function(result,status,jqXHR){
                     for (var i = 0; i < result.length; i++) {
                         var p = result[i];
+                        alert(p.cantidad);
                         $('#cantidad_asistencia').html("<h1 style='text-align:center'>"+p.cantidad+"</h1>"); 
                         $('#cantidad_asistencia_text').html("<h2 style='text-align:center'>Cantidad de Estudiantes</h2>");
                     }
